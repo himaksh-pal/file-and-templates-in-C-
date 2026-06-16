cc69a2fb04d8a84c5e7feac8a7dded1d1e16ad1c
himaksh-pal <palhimaksh@gmail.com>
2026-06-16 15:18:53 +0530

    added read and write

---
+++ Reading_and_Writing_in_File.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <fstream>
+using namespace std;
+int main(){
+    string st = "Himaksh ";
+    // opening file using constructors and writing in it
+    // ofstream out("sample.txt"); // write operation
+    // out<<st;
+    
+    string st2 ;
+    // opening the file using constructor and reading it
+    ifstream in("sample2.txt");
+    // in>>st2; --> this code will only read one word
+    getline(in,st2);
+    cout<<st2;
+
+
+
+    return 0;
+}
