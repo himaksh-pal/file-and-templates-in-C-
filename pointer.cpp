381b4f138a42f7740eff38a4bfa5f1094c911a58
himaksh-pal <palhimaksh@gmail.com>
2026-06-24 13:11:46 +0530

    C++

--- pointer.cpp
+++ pointer.cpp
@@ -4,8 +4,10 @@ int main(){
     int a=3;
     int* b=&a;
     cout<<"The address of a is :"<<b<<endl;
+    // & is used as the address of the operator
     cout<<"The address of a is :"<<&a<<endl;
-
+    
+    // * is used as the at that pointer 
     cout<<"The value of variable whose address is b is :"<<*b<<endl;
     return 0;
 }
